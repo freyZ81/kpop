880ff65f9329c270729a43320a7fb50c1edf300c
freyZ81 <freddybalbour02@web.de>
2024-01-20 00:41:34 +0100

    Sortiermöglichkeiten wurden hinzugefügt

--- allmemberlist.js
+++ allmemberlist.js
@@ -8,24 +8,155 @@ function fuehrendeNullWennEinstellig(num) {
 }
 
 function setMembers(memberArray) {
+    var tableBody = document.getElementById("tableBody");
     for (let i = 0; i < memberArray.length; i++) {
         var member = memberArray[i];
         memberbirthday = new Date(member.birthday);
-        const newRow = tableMembersGroupsAndBirthdays.insertRow(-1); // -1 fügt die Zeile am Ende der Tabelle ein
+        const newRow = tableBody.insertRow(-1); // -1 fügt die Zeile am Ende der Tabelle ein
         
         const countryCell = newRow.insertCell(0);
         countryCell.innerHTML = member.name;
     
         const countryCell2 = newRow.insertCell(-1);
         countryCell2.innerHTML = member.group[0];
-        
+
         const countryCell3 = newRow.insertCell(-1);
+        //countryCell3.innerHTML = birthdayParts;
         countryCell3.innerHTML = fuehrendeNullWennEinstellig(memberbirthday.getDate())
         + "." + fuehrendeNullWennEinstellig(memberbirthday.getMonth()+1)
         + "." + memberbirthday.getFullYear();
       }
+}
+
+function sortName() {
+    console.log("Sort Group");
+    allMembers.sort(function(a, b) {
+        var dateA = new Date(a.birthday);//parseDate(a.birthdate);
+        var dateB = new Date(b.birthday);//parseDate(b.birthdate);
+
+        // Zuerst nach Name sortieren
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+
+        // Zuerst nach Jahr sortieren
+        if (dateA.getYear() < dateB.getYear()) {
+            return -1;
+        }
+        if (dateA.getYear() > dateB.getYear()) {
+            return 1;
+        }
+
+        // Dann nach Monat sortieren
+        if (dateA.getMonth() < dateB.getMonth()) {
+            return -1;
+        }
+        if (dateA.getMonth() > dateB.getMonth()) {
+            return 1;
+        }
     
-     
+        // Dann nach Tag sortieren
+        if (dateA.getDate() < dateB.getDate()) {
+            return -1;
+        }
+        if (dateA.getDate() > dateB.getDate()) {
+            return 1;
+        }
+
+        
+    
+        // Wenn Monat und Tag gleich sind, keine Änderung vornehmen
+        return 0;
+    });
+    tableBody.innerHTML = '';
+    setMembers(allMembers);
+}
+
+function sortGroup() {
+    console.log("Sort Group");
+    allMembers.sort(function(a, b) {
+        var dateA = new Date(a.birthday);//parseDate(a.birthdate);
+        var dateB = new Date(b.birthday);//parseDate(b.birthdate);
+    
+        // Zuerst nach Gruppe sortieren
+        if (a.group < b.group) {
+            return -1;
+        }
+        if (a.group > b.group) {
+            return 1;
+        }
+
+        
+        // Zuerst nach Monat sortieren
+        if (dateA.getMonth() < dateB.getMonth()) {
+            return -1;
+        }
+        if (dateA.getMonth() > dateB.getMonth()) {
+            return 1;
+        }
+    
+        // Dann nach Tag sortieren
+        if (dateA.getDate() < dateB.getDate()) {
+            return -1;
+        }
+        if (dateA.getDate() > dateB.getDate()) {
+            return 1;
+        }
+
+        // Dann nach Jahr sortieren
+        if (dateA.getYear() < dateB.getYear()) {
+            return -1;
+        }
+        if (dateA.getYear() > dateB.getYear()) {
+            return 1;
+        }
+        
+    
+        // Wenn Monat und Tag gleich sind, keine Änderung vornehmen
+        return 0;
+    });
+    tableBody.innerHTML = '';
+    setMembers(allMembers);
+}
+
+function sortDate() {
+    console.log("Sort Date");
+    allMembers.sort(function(a, b) {
+        var dateA = new Date(a.birthday);//parseDate(a.birthdate);
+        var dateB = new Date(b.birthday);//parseDate(b.birthdate);
+    
+        // Zuerst nach Monat sortieren
+        if (dateA.getMonth() < dateB.getMonth()) {
+            return -1;
+        }
+        if (dateA.getMonth() > dateB.getMonth()) {
+            return 1;
+        }
+    
+        // Dann nach Tag sortieren
+        if (dateA.getDate() < dateB.getDate()) {
+            return -1;
+        }
+        if (dateA.getDate() > dateB.getDate()) {
+            return 1;
+        }
+
+        // Dann nach Jahr sortieren
+        if (dateA.getYear() < dateB.getYear()) {
+            return -1;
+        }
+        if (dateA.getYear() > dateB.getYear()) {
+            return 1;
+        }
+    
+        // Wenn Monat und Tag gleich sind, keine Änderung vornehmen
+        return 0;
+    });
+    tableBody.innerHTML = '';
+    setMembers(allMembers);
 }
 
 setMembers(allMembers)
