50d0f3faf3f23495bfa456f556782702539d29fc
freyZ81 <freddybalbour02@web.de>
2024-01-22 20:08:08 +0100

    * Member, die in den nächsten Tagen Geburtstag, werden jetzt angezeigt

--- allmemberlist.js
+++ allmemberlist.js
@@ -2,6 +2,8 @@
 allMembers = JSON.parse(localStorage.getItem('membersArray'));
 
 const tableMembersGroupsAndBirthdays = document.getElementById("tableMembersGroupsAndBirthdays");
+let todaysBirthdays = document.getElementById("birthdays");
+let nextBirthdays = [];
 
 function fuehrendeNullWennEinstellig(num) {
     return num < 10 ? "0" + num : num;
@@ -126,8 +128,8 @@ function sortGroup() {
 function sortDate() {
     console.log("Sort Date");
     allMembers.sort(function(a, b) {
-        var dateA = new Date(a.birthday);//parseDate(a.birthdate);
-        var dateB = new Date(b.birthday);//parseDate(b.birthdate);
+        var dateA = new Date(a.birthday);
+        var dateB = new Date(b.birthday);
     
         // Zuerst nach Monat sortieren
         if (dateA.getMonth() < dateB.getMonth()) {
@@ -161,7 +163,6 @@ function sortDate() {
 }
 
 function setBirthdays() {
-    let todaysBirthdays = document.getElementById("birthdays");
     todaysBirthdays.innerHTML = "";
 
 
@@ -178,23 +179,81 @@ function setBirthdays() {
     
         if (koreaDate.getDate() === memberbirthday.getDate() && koreaDate.getMonth() === memberbirthday.getMonth()) {
             if (member.group != "") {
-                todaysBirthdays.innerHTML += ("Heute hat " + member.name + " aus " + member.group + " Geburtstag. Sie wird " + (koreaDate.getYear()-memberbirthday.getYear()) + " Jahre alt.<br>");
+                todaysBirthdays.innerHTML += ("Heute hat " + member.name + " aus " + member.group + " Geburtstag. " + member.name + " wird " + (koreaDate.getYear()-memberbirthday.getYear()) + " Jahre alt.<br>");
             }
             if (member.group == "") {
-                todaysBirthdays.innerHTML += ("Heute hat " + member.name + " Geburtstag. Sie wird " + (koreaDate.getYear()-memberbirthday.getYear()) + " Jahre alt.<br>");
+                todaysBirthdays.innerHTML += ("Heute hat " + member.name + " Geburtstag. " + member.name + " wird " + (koreaDate.getYear()-memberbirthday.getYear()) + " Jahre alt.<br>");
             }
         }
-        //console.log(koreaDate.getDate());
         // Aktuelles Datum erstellen
         const currentDate2 = new Date();
 
-        // 7 Tage (in Millisekunden) zum aktuellen Datum hinzufügen
-        const futureDate = new Date(koreaDate.getTime() + 1 * 24 * 60 * 60 * 1000);
+        // x Tage (in Millisekunden) zum aktuellen Datum hinzufügen
+        const futureDate = new Date(koreaDate.getTime() + 4 * 24 * 60 * 60 * 1000);
 
-        //console.log('Zukünftiges Datum:', futureDate.getDate());
-
+        if (memberbirthday.getDate() > koreaDate.getDate()
+            && memberbirthday.getDate() < futureDate.getDate()
+            && memberbirthday.getMonth()+1 == koreaDate.getMonth()+1) {
+            nextBirthdays.push(member);
+        }   
     }
 }
 
+function setNextBirthdays(membersWithNextBirthdays) {
+    membersWithNextBirthdays.sort(function(a, b) {
+        var dateA = new Date(a.birthday);
+        var dateB = new Date(b.birthday);
+    
+        // Zuerst nach Monat sortieren
+        if (dateA.getMonth() < dateB.getMonth()) {
+            return -1;
+        }
+        if (dateA.getMonth() > dateB.getMonth()) {
+            return 1;
+        }
+    
+        // Dann nach Tag sortieren
+        if (dateA.getDate() < dateB.getDate()) {
+            return -1;
+        }
+        if (dateA.getDate() > dateB.getDate()) {
+            return 1;
+        }
+
+        // Dann nach Jahr sortieren
+        if (dateA.getYear() < dateB.getYear()) {
+            return -1;
+        }
+        if (dateA.getYear() > dateB.getYear()) {
+            return 1;
+        }
+    
+        // Wenn Monat und Tag gleich sind, keine Änderung vornehmen
+        return 0;
+    });
+
+    for (let i = 0; i < membersWithNextBirthdays.length; i++) {
+        var member = membersWithNextBirthdays[i];
+        memberbirthday = new Date(member.birthday);
+        
+        // Zeitzone für Südkorea festlegen
+        const koreaTimezone = 'Asia/Seoul';
+        // Aktuelles Datum und Uhrzeitobjekt erstellen
+        const currentDate = new Date();
+        // Zeitzone für das Datumobjekt festlegen
+        const koreaDate = new Date(currentDate.toLocaleString('en-US', {timeZone: koreaTimezone}));
+    
+        if (member.group != "") {
+            todaysBirthdays.innerHTML += ("Am " + memberbirthday.getDate() + "." + memberbirthday.getMonth()+1 + " hat " + member.name + " aus " + member.group + " Geburtstag. " + member.name + " wird " + (koreaDate.getYear()-memberbirthday.getYear()) + " Jahre alt.<br>");
+        }
+        if (member.group == "") {
+            todaysBirthdays.innerHTML += ("Am " + memberbirthday.getDate() + "." + memberbirthday.getMonth()+1 + " hat " + member.name + " Geburtstag. " + member.name + " wird " + (koreaDate.getYear()-memberbirthday.getYear()) + " Jahre alt.<br>");
+        }
+    }
+
+}
+
+
 setBirthdays();
+setNextBirthdays(nextBirthdays)
 setMembers(allMembers);
