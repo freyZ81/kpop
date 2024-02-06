ef29ad3d10f39530fa82d44a9a1c9ad0424f07a3
freyZ81 <freddybalbour02@web.de>
2024-02-06 13:05:02 +0100

    * Erstes Layout fürs member nennen erstellt

---
+++ nameGroupMembers.js
@@ -0,0 +1,7 @@
+// Werte aus dem Local Storage abrufen und verwenden
+allMembers = JSON.parse(localStorage.getItem('membersArray'));
+
+const question = document.getElementById("question")
+const inputGroupMember = document.getElementById("inputGroupMember")
+const tableMembers = document.getElementById("tableMembers")
+
